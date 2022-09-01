c4685e30231911b93c5dd56eb0d755ff3801169a
Man Cheng <cyrilcheng@protonmail.com>
2022-08-31 20:11:35 -0700

    commit tree c376df476d76fd86892482383b8196dc1afe0d7b

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+
+
+function superbowlWin(wingame){
+for(const record of wingame){
+    if(record.result === "W"){
+        return(record.year)}
+}
+}
+
+record.find(superbowlWin);
